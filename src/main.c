9e14101d381a85eb30e7cef2acae0bfedb37b70e
Tobias <tbraun@inf>
2014-06-19 17:53:10 +0200

    Cleanup not needed files

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include "stm32f10x.h"
 #include "stm32f10x_it.h"
 #include "stm32f10x_conf.h"
+#include "system_stm32f10x.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
