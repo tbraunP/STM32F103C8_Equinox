1bfa4f622b60d01f3738cca8698bcc61606dfaf9
Tobias <tbraun@inf>
2014-06-19 22:51:45 +0200

    GPIO Test

--- src/main.c
+++ src/main.c
@@ -13,65 +13,20 @@ int main(void)
 {
 
       /* GPIOD Periph clock enable */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
-    /* Configure PD0 and PD2 in output pushpull mode */
+    /* Configure pins in output pushpull mode */
     GPIO_StructInit(&GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
   
     while(1)
     {
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
-
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
-
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
-
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
-
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
-
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
-
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
-
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
-
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
-
-      /* Set PD0 and PD2 */
-      GPIOD->BSRR = 0x00000005;
-      /* Reset PD0 and PD2 */
-      GPIOD->BRR  = 0x00000005;
+      GPIO_SetBits(GPIOA, GPIO_Pin_0);
+      for (int i = 0; i< 4000;i++);
+      GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+      for (int i = 0; i< 4000;i++);
     }
 }
